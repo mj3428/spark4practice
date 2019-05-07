92cee42f147b30cd045a943c30b9b02165197595
mj3428 <38501129+mj3428@users.noreply.github.com>
2019-05-07 11:17:04 +0800

    Update RDD_cal.py

--- RDD_cal.py
+++ RDD_cal.py
@@ -1,3 +1,4 @@
+# 1
 from pyspark import SparkConf, SparkContext
 
 # conf = SparkConf().setMaster("local").setAppName("My App")
@@ -8,3 +9,21 @@ squared = nums.map(lambda x:x * x).collect()
 for num in squared:
     print("%i" %(num))
 # 返回的是序列的迭代器 输出的RDD并不是迭代器，而是一个包含各个迭代器可访问的所有元素的RDD
+
+# 2
+from pyspark import SparkContext
+
+sc = SparkContext.getOrCreate()
+
+lines = sc.parallelize(["hello world", "hi"])
+words = lines.flatMap(lambda line: line.split(" "))
+print(words.first()) # 返回hello
+
+# 总结原理
+'''
+RDD1:{"coffee panda","happy panda","happiest panda party"}
+——>rdd1.map(tokenize):{["coffee","panda"],["happy","panda"],["happiest","panda","party"]}
+
+RDD1:同上
+——>rdd1.flatMap(tokenize):{"coffee","panda","happy","panda","happiest","panda","party"}
+'''

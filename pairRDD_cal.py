6128fb61d0dc2ffa1698607580c6804beb63605c
mj3428 <38501129+mj3428@users.noreply.github.com>
2019-05-10 08:52:04 +0800

    Update pairRDD_cal.py

--- pairRDD_cal.py
+++ pairRDD_cal.py
@@ -9,3 +9,16 @@ pairs = lines.map(lambda x: (x.split(" ")[0], x))
 ["hello world", "hi"]--->[('hello', 'hello world'), ('hi', 'hi')] 单个字母以自身为键，同时以自身为值
 ["hello world", "hi man"]--->[('hello', 'hello world'), ('hi', 'hi man')]
 '''
+
+# 聚合操作
+rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+# \\mapvalues遍历成元组 reduceByKey是聚合，形成新的RDD\\
+# MapReduce中的合成器（combiner），调用reduceByKey()和foldByKey()会在为每个键计算全局的总结果之前先自动在每台机器上进行本地合并
+
+# 简单的单词统计（实际也是用聚合）
+lines = sc.parallelize(["hello world", "hi man"])
+words = lines.flatMap(lambda x: x.split(" "))
+result = words.map(lambda x: (x,1)).reduceByKey(lambda x, y: x + y)
+# \\output:[('hello', 1), ('world', 1), ('hi', 1), ('man', 1)]
+# 当然更快的方式可以尝试
+input.flatMap(x => x.plit(" ")).countByValue()

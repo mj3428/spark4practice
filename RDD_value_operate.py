5cc69f2eb7ecae87b0cec9c4524861c5995f58e9
mj3428 <38501129+mj3428@users.noreply.github.com>
2019-05-23 11:28:01 +0800

    Update RDD_value_operate.py

--- RDD_value_operate.py
+++ RDD_value_operate.py
@@ -4,4 +4,12 @@ Spark的数值操作是通过流式算法实现的，允许以每次一个元素
 接下来，
 我们会使用汇总统计来从数据中移除一些异常值。由于我们会两次使用同一个RDD（一次用来计算汇总统计数据，另一次用来移除异常值）
 '''
-# ppython移除异常值
+
+# python移除异常值
+# 要把String类型RDD转为数字数据，这样才能使用统计函数并移除异常值
+distanceNumerics = distances.map(lambda string: float(string))
+stats = distanceNumerics.stats()
+stddev = stats.stdev()
+mean = stats.mean()
+reasonableDistances = distanceNumerics.filter(lambda x: math.fabs(x - mean) < 3 * stddev)
+print(reasonableDistances.collect())
